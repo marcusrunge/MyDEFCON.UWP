b50b9a246385ff53f629677053dfddc19f875a4e
Marcus Runge <marcus.runge@outlook.com>
2018-07-14 20:29:59 +0200

    2.5.6.0a TODO "The transfer could not be performed due to a clock stretch timeout. Make sure the clock line is not being held low by a slave device."

--- MyDEFCON_UWP/ViewModels/FullScreenPageViewModel.cs
+++ MyDEFCON_UWP/ViewModels/FullScreenPageViewModel.cs
@@ -5,9 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Template10.Mvvm;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
 using Windows.Storage;
 using Windows.UI.Core;
+using Windows.UI.Input;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 
 namespace MyDEFCON_UWP.ViewModels
@@ -15,12 +19,15 @@ namespace MyDEFCON_UWP.ViewModels
     public class FullScreenPageViewModel : ViewModelBase
     {
         DatagramService _datagramService;
+        UIElement _uIElement;
+        I2cDevice _i2CDevice;
+        double _onPointerPressedY, _onPointerReleasedY;
         bool _useTransparentTile = default(bool);
         bool _isFullScreen = default(bool);
         int _defconStatus;
         public int DefconStatus { get { return _defconStatus; } set { Set(ref _defconStatus, value); } }
         double _fontSize;
-        public double FontSize { get { return _fontSize; } set { Set(ref _fontSize, value); } }        
+        public double FontSize { get { return _fontSize; } set { Set(ref _fontSize, value); } }
         VisualState _defconVisualState;
         public VisualState DefconVisualState { get { return _defconVisualState; } set { Set(ref _defconVisualState, value); } }
 
@@ -55,6 +62,16 @@ namespace MyDEFCON_UWP.ViewModels
                         LiveTileService.SetLiveTile(_defconStatus, _useTransparentTile);
                     }
                 };
+            }
+
+            //Raspi ONLY
+            string i2cDeviceSelector = I2cDevice.GetDeviceSelector();
+            I2cConnectionSettings i2CConnectionSettings = new I2cConnectionSettings(0x45);
+            IReadOnlyList<DeviceInformation> deviceInformationCollection = await DeviceInformation.FindAllAsync(i2cDeviceSelector);
+            if (deviceInformationCollection.Count > 0)
+            {
+                var i2CDevice = await I2cDevice.FromIdAsync(deviceInformationCollection[0].Id, i2CConnectionSettings);
+                _i2CDevice = i2CDevice;
             }
         }
 
@@ -129,5 +146,37 @@ namespace MyDEFCON_UWP.ViewModels
                     _isFullScreen = true;
                 }
             }, () => true));
+
+        DelegateCommand<PointerRoutedEventArgs> _onPointerPressedCommand;
+        public DelegateCommand<PointerRoutedEventArgs> OnPointerPressedCommand
+            => _onPointerPressedCommand ?? (_onPointerPressedCommand = new DelegateCommand<PointerRoutedEventArgs>((e) =>
+            {
+                _uIElement = e.OriginalSource as UIElement;
+                _onPointerPressedY = e.GetCurrentPoint(_uIElement).Position.Y;
+            }, (e) => true));
+
+        DelegateCommand<PointerRoutedEventArgs> _onPointerReleasedCommand;
+        public DelegateCommand<PointerRoutedEventArgs> OnPointerReleasedCommand
+            => _onPointerReleasedCommand ?? (_onPointerReleasedCommand = new DelegateCommand<PointerRoutedEventArgs>((e) =>
+            {
+                _onPointerReleasedY = e.GetCurrentPoint(_uIElement).Position.Y;
+                double deltaY = _onPointerReleasedY - _onPointerPressedY;
+                if (deltaY > 0 && deltaY > 10) ToggleScreenBacklight(false);
+                if (deltaY < 0 && deltaY < -10) ToggleScreenBacklight(true);
+            }, (e) => true));
+
+        private void ToggleScreenBacklight(bool isScreenBacklightOn)
+        {
+            byte brightness = isScreenBacklightOn ? (byte)255 : (byte)16;
+            try
+            {
+                //TODO "The transfer could not be performed due to a clock stretch timeout. Make sure the clock line is not being held low by a slave device."
+                //_i2CDevice?.Write(new byte[] { 0x86, brightness });
+            }
+            catch (Exception e)
+            {
+                //System.Diagnostics.Debug.WriteLine(e.Source + ", " + e.Message);
+            }                        
+        }
     }
 }
