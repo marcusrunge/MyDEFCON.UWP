40cd072a6577510ba88c4357e2a4bce6b6add0cd
Marcus Runge <marcus.runge@outlook.com>
2020-03-28 20:20:17 +0100

    fontsize binding

--- MyDEFCON_UWP/ViewModels/MainViewModel.cs
+++ MyDEFCON_UWP/ViewModels/MainViewModel.cs
@@ -1,12 +1,25 @@
 ﻿
 using MyDEFCON_UWP.Helpers;
+using System;
+using System.Windows.Input;
+using Windows.UI.Xaml;
 
 namespace MyDEFCON_UWP.ViewModels
 {
     public class MainViewModel : Observable
     {
+        private double _fontSize;
+        public double FontSize { get => _fontSize; set => Set(ref _fontSize, value); }
+
         public MainViewModel()
         {
-        }
+            FontSize = 72;
+        }                
+
+        private ICommand _setFontSizeCommand;
+        public ICommand SetFontSizeCommand => _setFontSizeCommand ?? (_setFontSizeCommand = new RelayCommand<object>((param) =>
+        {
+           FontSize = Math.Floor((param as SizeChangedEventArgs).NewSize.Height /8);           
+        }));
     }
 }
