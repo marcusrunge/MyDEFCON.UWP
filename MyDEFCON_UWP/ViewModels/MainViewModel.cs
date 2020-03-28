8e3b4c1250bc4c33bf008b52f59b6b77e1d06f29
Marcus Runge <marcus.runge@outlook.com>
2020-03-28 20:41:25 +0100

    fontsize

--- MyDEFCON_UWP/ViewModels/MainViewModel.cs
+++ MyDEFCON_UWP/ViewModels/MainViewModel.cs
@@ -13,13 +13,15 @@ namespace MyDEFCON_UWP.ViewModels
 
         public MainViewModel()
         {
-            FontSize = 72;
-        }                
+            FontSize = 14;
+        }
 
         private ICommand _setFontSizeCommand;
         public ICommand SetFontSizeCommand => _setFontSizeCommand ?? (_setFontSizeCommand = new RelayCommand<object>((param) =>
         {
-           FontSize = Math.Floor((param as SizeChangedEventArgs).NewSize.Height /8);           
+            double calculatedTextBlockWidth = (param as SizeChangedEventArgs).NewSize.Height * 0.5 * 6.5;
+            if ((param as SizeChangedEventArgs).NewSize.Width > calculatedTextBlockWidth) FontSize = Math.Floor((param as SizeChangedEventArgs).NewSize.Height * 0.5);
+            else FontSize = Math.Floor((param as SizeChangedEventArgs).NewSize.Width / 6.5);
         }));
     }
 }
