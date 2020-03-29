a48a22e850349797376b2616dd55a140e995bf30
Marcus Runge <marcus.runge@outlook.com>
2020-03-29 21:06:32 +0200

    saving and retrieving status

---
+++ MyDEFCON_UWP/Converter/StatusToBoolConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace MyDEFCON_UWP.Converter
+{
+    public class StatusToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) => (int)value == int.Parse(parameter as string);
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MyDEFCON_UWP/ViewModels/MainViewModel.cs
+++ MyDEFCON_UWP/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿
 using MyDEFCON_UWP.Helpers;
+using Services;
 using System;
 using System.Windows.Input;
 using Windows.UI.Xaml;
@@ -10,6 +11,9 @@ namespace MyDEFCON_UWP.ViewModels
     {
         private double _fontSize;
         public double FontSize { get => _fontSize; set => Set(ref _fontSize, value); }
+
+        private int _defconStatus;
+        public int DefconStatus { get => _defconStatus; set => Set(ref _defconStatus, value); }
 
         public MainViewModel()
         {
@@ -22,6 +26,19 @@ namespace MyDEFCON_UWP.ViewModels
             double calculatedTextBlockWidth = (param as SizeChangedEventArgs).NewSize.Height * 0.5 * 6.5;
             if ((param as SizeChangedEventArgs).NewSize.Width > calculatedTextBlockWidth) FontSize = Math.Floor((param as SizeChangedEventArgs).NewSize.Height * 0.5);
             else FontSize = Math.Floor((param as SizeChangedEventArgs).NewSize.Width / 6.5);
+        }));
+
+        private ICommand _setDefconStatusCommand;
+        public ICommand SetDefconStatusCommand => _setDefconStatusCommand ?? (_setDefconStatusCommand = new RelayCommand<object>((param) =>
+        {
+            StorageService.SetSetting("defconStatus", (string)param, StorageService.StorageStrategies.Roaming);
+            DefconStatus = int.Parse(param as string);
+        }));
+
+        private ICommand _loadedCommand;
+        public ICommand LoadedCommand => _loadedCommand ?? (_loadedCommand = new RelayCommand<object>((param) =>
+        {            
+            DefconStatus = int.Parse(StorageService.GetSetting("defconStatus", "5", StorageService.StorageStrategies.Roaming));
         }));
     }
 }
