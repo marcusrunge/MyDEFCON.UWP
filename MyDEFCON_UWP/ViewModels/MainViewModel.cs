fab2b771e75af2198be17417ded6645650e41443
Marcus Runge <marcus.runge@outlook.com>
2020-03-31 20:04:20 +0200

    ADDED status sharing

--- MyDEFCON_UWP/ViewModels/MainViewModel.cs
+++ MyDEFCON_UWP/ViewModels/MainViewModel.cs
@@ -3,20 +3,24 @@ using MyDEFCON_UWP.Helpers;
 using Services;
 using System;
 using System.Windows.Input;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 
 namespace MyDEFCON_UWP.ViewModels
 {
     public class MainViewModel : Observable
     {
+        private IEventService _eventService;
         private double _fontSize;
         public double FontSize { get => _fontSize; set => Set(ref _fontSize, value); }
 
         private int _defconStatus;
         public int DefconStatus { get => _defconStatus; set => Set(ref _defconStatus, value); }
 
-        public MainViewModel()
+        public MainViewModel(IEventService eventService)
         {
+            _eventService = eventService;
             FontSize = 14;
         }
 
@@ -39,6 +43,27 @@ namespace MyDEFCON_UWP.ViewModels
         public ICommand LoadedCommand => _loadedCommand ?? (_loadedCommand = new RelayCommand<object>((param) =>
         {            
             DefconStatus = int.Parse(StorageService.GetSetting("defconStatus", "5", StorageService.StorageStrategies.Roaming));
+            DataTransferManager.GetForCurrentView().DataRequested += MainViewModel_DataRequested;
+            _eventService.AppBarButtonClicked += (s, e) =>
+            {
+                if((e as AppBarButtonClickedEventArgs).Button.Equals("Share")) DataTransferManager.ShowShareUI();
+            };
+        }));
+
+        private void MainViewModel_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var dataPackage = args.Request.Data;
+            var deferral = args.Request.GetDeferral();
+            dataPackage.Properties.Title = "DEFCON STATUS";
+            dataPackage.Properties.Description = "DEFCON Status Payload for sharing";            
+            dataPackage.SetBitmap(RandomAccessStreamReference.CreateFromUri(new Uri(string.Format("ms-appx:///ShareImages/Defcon{0}.png", _defconStatus.ToString()))));
+            deferral.Complete();
+        }
+
+        private ICommand _unloadedCommand;
+        public ICommand UnloadedCommand => _unloadedCommand ?? (_unloadedCommand = new RelayCommand<object>((param) =>
+        {
+            DataTransferManager.GetForCurrentView().DataRequested -= MainViewModel_DataRequested;
         }));
     }
 }
