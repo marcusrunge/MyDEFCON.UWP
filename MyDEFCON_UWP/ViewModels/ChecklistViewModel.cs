51d31f1976102e9d421dd285ec46f625ebe1d0ec
Marcus Runge <marcus.runge@outlook.com>
2020-04-04 15:41:27 +0200

    ADDED buttons

--- MyDEFCON_UWP/ViewModels/ChecklistViewModel.cs
+++ MyDEFCON_UWP/ViewModels/ChecklistViewModel.cs
@@ -1,12 +1,39 @@
 ﻿
 using MyDEFCON_UWP.Helpers;
+using Services;
+using System;
+using System.Windows.Input;
+using Windows.UI.Xaml;
 
 namespace MyDEFCON_UWP.ViewModels
 {
     public class ChecklistViewModel : Observable
     {
+        private int _defconStatus;
+        public int DefconStatus { get => _defconStatus; set => Set(ref _defconStatus, value); }
+
+        private double _fontSize;
+        public double FontSize { get => _fontSize; set => Set(ref _fontSize, value); }
+
         public ChecklistViewModel()
         {
+            FontSize = 14;
         }
-    }
-}
+
+        private ICommand _loadedCommand;
+        public ICommand LoadedCommand => _loadedCommand ?? (_loadedCommand = new RelayCommand<object>((param) =>
+        {
+            DefconStatus = int.Parse(StorageService.GetSetting("defconStatus", "5", StorageService.StorageStrategies.Roaming));            
+        }));
+
+        private ICommand _setFontSizeCommand;
+        public ICommand SetFontSizeCommand => _setFontSizeCommand ?? (_setFontSizeCommand = new RelayCommand<object>((param) =>
+        {
+            FontSize = Math.Floor((param as SizeChangedEventArgs).NewSize.Width / 6.5);
+        }));
+
+        private ICommand _loadDefconChecklistCommand;
+        public ICommand LoadDefconChecklistCommand => _loadDefconChecklistCommand ?? (_loadDefconChecklistCommand = new RelayCommand<object>((param) =>
+        {            
+        }));
+    }}
