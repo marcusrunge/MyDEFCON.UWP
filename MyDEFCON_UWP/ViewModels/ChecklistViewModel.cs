ca9a43d7bf96fe35995b4f2edacae895f5443216
Marcus Runge <marcus.runge@outlook.com>
2020-04-04 19:01:32 +0200

    Added add

--- MyDEFCON_UWP/ViewModels/ChecklistViewModel.cs
+++ MyDEFCON_UWP/ViewModels/ChecklistViewModel.cs
@@ -10,6 +10,9 @@ namespace MyDEFCON_UWP.ViewModels
 {
     public class ChecklistViewModel : Observable
     {
+        private IEventService _eventService;
+        double _textBoxWidth;
+
         private int _defconStatus;
         public int DefconStatus { get => _defconStatus; set => Set(ref _defconStatus, value); }
 
@@ -19,8 +22,9 @@ namespace MyDEFCON_UWP.ViewModels
         ItemObservableCollection<CheckListItem> _defconCheckList;
         public ItemObservableCollection<CheckListItem> DefconCheckList { get { return _defconCheckList; } set { Set(ref _defconCheckList, value); } }
 
-        public ChecklistViewModel()
+        public ChecklistViewModel(IEventService eventService)
         {
+            _eventService = eventService;
             DefconStatus = int.Parse(StorageService.GetSetting("defconStatus", "5", StorageService.StorageStrategies.Roaming));
             FontSize = 14;
         }
@@ -29,7 +33,25 @@ namespace MyDEFCON_UWP.ViewModels
         public ICommand LoadedCommand => _loadedCommand ?? (_loadedCommand = new RelayCommand<object>(async (param) =>
         {            
             DefconCheckList= await CheckListService.LoadCheckList(DefconStatus);
+            _eventService.AppBarButtonClicked += AppBarButtonClicked;
         }));
+
+        private void AppBarButtonClicked(object sender, EventArgs e)
+        {
+            switch ((e as AppBarButtonClickedEventArgs).Button)
+            {
+                case "Add":
+                    AddItemToChecklist();                    
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void AddItemToChecklist()
+        {
+            DefconCheckList.Add(new CheckListItem() { Item = string.Empty, Checked = false, DefconStatus = (short)DefconStatus, FontSize=FontSize, UnixTimeStampCreated= DateTimeOffset.Now.ToUnixTimeMilliseconds(), Deleted = false, Visibility = Visibility.Visible , Width=_textBoxWidth});
+        }
 
         private ICommand _setFontSizeCommand;
         public ICommand SetFontSizeCommand => _setFontSizeCommand ?? (_setFontSizeCommand = new RelayCommand<object>((param) =>
@@ -48,6 +70,12 @@ namespace MyDEFCON_UWP.ViewModels
         private ICommand _unloadedCommand;
         public ICommand UnloadedCommand => _unloadedCommand ?? (_unloadedCommand = new RelayCommand<object>((param) =>
         {            
+        }));
+
+        private ICommand _windowSizeChangedCommand;
+        public ICommand WindowSizeChangedCommand => _windowSizeChangedCommand ?? (_windowSizeChangedCommand = new RelayCommand<SizeChangedEventArgs>((param) =>
+        {
+            _textBoxWidth = (param.NewSize.Width - 52);
         }));
     }
 }
