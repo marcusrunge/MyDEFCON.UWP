bec2c3047534a62c3436ace5246e248ecbe7daad
Marcus Runge <marcus.runge@outlook.com>
2020-06-14 22:11:19 +0200

    ADDED basic storage component

---
+++ Storage/StorageFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Storage
+{
+    public class StorageFactory
+    {
+        private static IStorage _storage;
+        public static IStorage Create() => _storage ?? (_storage = new Storage());
+    }
+}
