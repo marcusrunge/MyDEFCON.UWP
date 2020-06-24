23a4fa7e66f7988e4a356884ea9548c5dc6f2d14
Marcus Runge <marcus.runge@outlook.com>
2020-06-24 16:49:30 +0200

    ADDED basic ToastNotifications component

---
+++ ToastNotifications/ToastNotificationsFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToastNotifications
+{
+    public class ToastNotificationsFactory
+    {
+        private static IToastNotifications _toastNotifications;
+        public static IToastNotifications Create() => _toastNotifications ?? (_toastNotifications = new ToastNotifications());
+    }
+}
