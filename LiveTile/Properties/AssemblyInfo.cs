303d67c250b3f2be24da5a549fefe634c264c38f
Marcus Runge <marcus.runge@outlook.com>
2021-03-27 20:41:59 +0100

    UPDATED NuGet

--- LiveTile/Properties/AssemblyInfo.cs
+++ LiveTile/Properties/AssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden, 
 // indem Sie "*" wie unten gezeigt eingeben:
 // [Assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.3.0")]
-[assembly: AssemblyFileVersion("3.0.3.0")]
+[assembly: AssemblyVersion("3.0.4.0")]
+[assembly: AssemblyFileVersion("3.0.4.0")]
 [assembly: ComVisible(false)]
